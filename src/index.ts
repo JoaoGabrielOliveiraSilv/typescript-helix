ca4ad26c0866abbae2319907ace2bf222ce65f0f
João Gabriel de Oliveira e Silva <60318307+JoaoGabrielOliveiraSilv@users.noreply.github.com>
2023-01-11 09:34:56 -0300

    Implement graphql-helix

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,48 @@
 import 'graphql-import-node';
-import { execute, parse } from 'graphql';
-import { schema } from './schema';
+import fastify from 'fastify';
+import { getGraphQLParameters, Request, processRequest, sendResult, renderGraphiQL, shouldRenderGraphiQL } from 'graphql-helix';
+import { schema } from './schemas/schema';
 
 async function main() {
-    const myQuery = parse(`query { info }`);
+    const server = fastify();
 
-    const result = await execute({
-        schema,
-        document: myQuery
+    server.route({
+        method: ["POST", "GET"],
+        url: "/graphql",
+        handler: async (req, res) => {
+            const request: Request = {
+                headers: req.headers,
+                method: req.method,
+                query: req.query,
+                body: req.body
+            }
+
+            if (shouldRenderGraphiQL(request)) {
+                res.header("Content-Type", "text/html");
+                res.send(
+                    renderGraphiQL({ endpoint: "/graphql" })
+                )
+
+                return;
+            }
+
+            const { operationName, query, variables } = getGraphQLParameters(request);
+
+            const result = await processRequest({
+                request,
+                schema,
+                operationName,
+                query,
+                variables
+            });
+
+            sendResult(result, res.raw);
+        }
     });
 
-    console.log(result);
+    server.listen({ port: 3000 }, () => {
+        console.log(`Server is running on http://localhost:3000/`);
+    });
 }
 
 main();
