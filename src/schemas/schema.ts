cae87461459782ff24967ce4a38a50a4563b959f
João Gabriel de Oliveira e Silva <60318307+JoaoGabrielOliveiraSilv@users.noreply.github.com>
2023-01-11 12:49:17 -0300

    Implementing mutation to create links

--- src/schemas/schema.ts
+++ src/schemas/schema.ts
@@ -2,14 +2,25 @@ import { makeExecutableSchema } from "@graphql-tools/schema"
 import { Link } from "../types";
 import typeDefs from "./schema.graphql";
 
-const LINKS: Link[] = [
+const links: Link[] = [
     { id: "1", url: "https://shouldideploy.today/", description: "SHOULD I DEPLOY TODAY?" }
 ]
 
 const resolvers = {
     Query: {
         info: () => 'Test',
-        feed: () => LINKS
+        feed: () => links
+    },
+    Mutation: {
+        post: (parent: unknown, args: { description: string, url: string }) => {
+            const newLink: Link = {
+                id: `link-${links.length}`,
+                ...args
+            };
+
+            links.push(newLink);
+            return newLink;
+        }
     }
 }
 
